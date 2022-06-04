eace9490d8c18d1fbd6e21f5b5ce2584767ea187
rijal <jeryzal333@gmail.com>
2022-06-05 06:53:45 +0700

    Lazy loading component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import About from "../views/About.vue";
+// import About from "../views/About.vue";
 import Jobs from "../views/jobs/Jobs.vue";
 import JobDetails from "../views/jobs/JobDetails.vue";
 import NotFound from "../views/NotFound.vue";
@@ -14,12 +14,12 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    component: About,
+    // component: About,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    // component: () =>
-    //   // import( webpackChunkName: "about"  "../views/About.vue"),
+    component: () =>
+        import(/* webpackChunkName: "about" */ "../views/jobs/Jobs.vue"),
   },
   {
     path: "/jobs",
