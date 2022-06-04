ab211f9cc94a3f107951fdc50a5c5f87f78e318f
rijal <jeryzal333@gmail.com>
2022-06-05 06:03:19 +0700

    folder structure, add one router page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Jobs from "../views/jobs/Jobs.vue";
 
 const routes = [
   {
@@ -10,11 +12,22 @@ const routes = [
   {
     path: "/about",
     name: "About",
+    component: About,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    // component: () =>
+    //   // import( webpackChunkName: "about"  "../views/About.vue"),
+  },
+  {
+    path: "/jobs",
+    name: "Jobs",
+    component: Jobs,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/jobs/Jobs.vue"),
   },
 ];
 
